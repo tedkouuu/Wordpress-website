fb63ffd80875fd41d81d30ee91a8db7a1951e6fb
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-10 16:40:53 +0200

    [Add] Added children pages

--- page.php
+++ page.php
@@ -16,21 +16,32 @@ the_post(); ?>
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
+
+    <?php 
+
+    $theParent = wp_get_post_parent_id(get_the_ID());
+
+    if ($theParent){ ?>
+        
+        <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i>Back to <?php echo get_the_title($theParent);?></a> <span class="metabox__main"><?php the_title();?></span>
         </p>
       </div>
 
-      <!--
+    <?php }
+
+    ?>
+    
+<!--       
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
--->
+      </div> -->
+
 
       <div class="generic-content">
         <?php the_content();?>
@@ -42,4 +53,5 @@ the_post(); ?>
 get_footer();
 
 ?>
+
 
