0b8530b3600678f5a429633c96340b3cf92b2820
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-11 17:49:54 +0200

    [Update] Coloring the current page

--- header.php
+++ header.php
@@ -17,7 +17,8 @@
           <nav class="main-navigation">
 
             <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <!-- If the current page is the About Us page or if the current page's parent page is the About Us page, then echo out the special class -->
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 12) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
