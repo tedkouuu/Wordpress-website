a8df33d9e83a234170d24d13faf7b014b0cd6623
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-13 16:52:23 +0200

    [Update] Updated the header and front page

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -6,6 +6,8 @@ they are automatically enabled. -->
 <?php function university_post_types() {
     register_post_type('event', array(
         // Describe what kind of post type you want
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
