c5322884af17f9ee0864d575525dcfdb454e3c87
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-15 14:57:35 +0200

    [Update] Added Program post type

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,26 +1,37 @@
-<!-- mu-plugins is a special directory where you can place plugins that 
-you want to be automatically activated for all sites in a multisite network. 
-Unlike regular plugins, you don't need to activate these plugins individually; 
-they are automatically enabled. -->
-
 <?php function university_post_types() {
-    register_post_type('event', array(
-        // Describe what kind of post type you want
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt'),
-        'rewrite' => array('slug' => 'events'),
-        'has_archive' => true,
-        'public' => true,
-        'labels' => array(  
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-        ),
-        'menu_icon' => 'dashicons-calendar'
-    ));
+
+register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+        'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+));
+
+register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+        'name' => 'Programs',
+        'add_new_item' => 'Add New Program',
+        'edit_item' => 'Edit Program',
+        'all_items' => 'All Programs',
+        'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+));
 }
-// Create new Post Type
 add_action('init', 'university_post_types');
 ?>
+
