7ab6143f8f317e1048e9e0f013e98bea2533c39b
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-15 16:14:48 +0200

    [Update] Displaying the Relationships between Events and Programs

--- finctional-university-theme/single-event.php
+++ finctional-university-theme/single-event.php
@@ -24,6 +24,23 @@ the_post(); ?>
   </div>
 
     <div class="generic content"><?php the_content(); ?></div>
+
+    <?php 
+    
+    $relatedPrograms = get_field('related_programs');
+
+    if ($relatedPrograms) {
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+      echo '<ul class="link-list min-list">';
+      foreach($relatedPrograms as $program){ ?>
+         <!-- $program returns array of post objects -->
+         <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+     <?php }
+     echo '</ul>';
+    }
+    
+    ?>
 </div>
 
 
