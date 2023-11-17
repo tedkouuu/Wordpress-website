5639222df8d9ddf53e681c795ae1c54909ce0fa6
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-17 15:51:38 +0200

    [Add] Added new directory for storing the duplicate code as template

--- finctional-university-theme/front-page.php
+++ finctional-university-theme/front-page.php
@@ -36,27 +36,15 @@
           ));
 
           while($homepageEvents->have_posts()){
-            $homepageEvents->the_post(); ?>
-
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php 
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate->format('M')
-              ?></span>
-              <span class="event-summary__day"><?php echo $eventDate->format('d')?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php if (has_excerpt()) {
-                echo get_the_excerpt();
-              } else {
-                echo wp_trim_words(get_the_content(), 18);
-              } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-
-          <?php }
+            $homepageEvents->the_post();
+            
+            /* 
+               The first argument points towards the file that I want to include 
+               Second argument is appended to the first argument and WP will search for a file with that name
+               (template-parts) folder with (content-event.php file)
+            */
+            get_template_part('template-parts/content', 'event');
+           }
 
           ?>
 
