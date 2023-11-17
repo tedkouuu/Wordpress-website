3f3aa6991973e67fe6e8ce183d0215499c901614
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-17 20:35:18 +0200

    [Update] Adding Google Maps to the Front-End

--- finctional-university-theme/functions.php
+++ finctional-university-theme/functions.php
@@ -38,6 +38,7 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=google api key' , NULL, '1.0', true);    
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -86,10 +87,16 @@ function university_adjust_queries($query){
         ));
    }
 }
-
+ 
 add_action('pre_get_posts', 'university_adjust_queries');
 
 
+function universityMapKey($api) {
+  $api['key'] = 'google api key';
+  return $api;
+}
+// acf(Advanced Custom Fields)
+add_filter('acf/fields/google_map/api', 'universityMapKey');
 ?> 
 
 
