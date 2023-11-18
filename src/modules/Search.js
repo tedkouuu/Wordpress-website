fc0461a9f295959ada545c67acd811f52154acfb
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-11-18 18:35:21 +0200

    [Update] Added loading spinner icon

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -4,12 +4,15 @@ class Search {
     // 1. describe and create/initate objects
     constructor(){
         // this refers to the search object that was instantiated
+        this.resultsDiv = $("#search-overlay__results");
         this.searchButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlayDiv = $(".search-overlay");
         this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousValue;
         this.typingTimer;
 
     }
@@ -23,21 +26,43 @@ class Search {
         this.searchButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
-        this.searchField.on("keydown", this.typingLogic.bind(this));
+        this.searchField.on("keyup", this.typingLogic.bind(this));
     }
     // 3. methods (function, action)
 
     typingLogic() {
-       clearTimeout(this.typingTimer);
-       this.typingTimer = setTimeout(function () {console.log("TEST TIMEOUT")}, 2000);
+
+       if (this.searchField.val() != this.previousValue) {
+        clearTimeout(this.typingTimer);
+
+        if (this.searchField.val()) {
+          if (!this.isSpinnerVisible) {
+            this.resultsDiv.html('<div class="spinner-loader"></div>');
+            this.isSpinnerVisible = true;
+         }
+           this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        } else {
+          this.resultsDiv.html('');
+          this.isSpinnerVisible = false;
+        }
+
+        }
+
+       this.previousValue = this.searchField.val();
+    }
+
+    getResults(){
+        this.resultsDiv.html("Imagine real search results here...");
+        this.isSpinnerVisible = false;
     }
 
     keyPressDispatcher(e){
        // e is a parameter holding information that was passed, about the key press event
 
-       if (e.keyCode == 83 && !this.isOverlayOpen){
+      // If any field is currently focused, the overlay will not show   
+       if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')){
         this.openOverlay();
-
+        
        }
 
        if (e.keyCode == 27 && this.isOverlayOpen){
